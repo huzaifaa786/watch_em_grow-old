ebf41f60aa41c8c7d5cb661e3d829bbd41da3964
HASEEB ULLAH TAHIR <tritechuzaifa@gmail.com>
2023-04-29 14:13:09 +0500

    Update  by Haseeb 29/4/23

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.brown,
       ),
-      initialRoute: 'customer',
+      initialRoute: 'orderDetail',
       routes: {
           'register': (context) => const RegisterScreen(),
           'profile': (context) => const ProfileScreen(),
